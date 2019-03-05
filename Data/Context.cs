2eab8bb0fabf28e5bfd7010cc07333cc1af0775d
marwalouani <marwa.alouani@esprit.tn>
2019-03-05 21:17:56 +0100

    Initial

---
+++ Data/Context.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+   public class Context:DbContext
+    {
+        public Context():base ("name=BD")
+        {
+
+
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+    //convention et config et startégie d'heritage
+        }
+    }
+}
---
+++ Data/Conventions/KeyConvention.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Conventions
+{
+   public class KeyConvention:Convention
+    {
+        //selon la convention que je veux
+        public KeyConvention()
+        {
+            Properties().Where(t => t.Name.EndsWith("Code")).Configure(t => t.IsKey());
+        }
+    }
+}
